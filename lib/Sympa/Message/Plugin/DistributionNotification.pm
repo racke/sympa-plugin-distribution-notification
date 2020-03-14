b6e30a8db99010ee012487fdfe41ff6297389fe8
Stefan Hornburg (Racke) <racke@linuxia.de>
2020-03-14 07:16:54 +0100

    Rename module.

--- lib/Sympa/Message/Plugin/DistributionNotification.pm
+++ lib/Sympa/Message/Plugin/DistributionNotification.pm
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package Sympa::Plugin::Message::DistributionNotification;
+package Sympa::Message::Plugin::DistributionNotification;
 
 use strict;
 use warnings;
